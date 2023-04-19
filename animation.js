ee69e207297fcf336097c1ad53054a2ce8be68fe
jeannie <jeanniezheng123@gmail.com>
2023-04-19 15:09:51 -0500

    Added creations slider and skills section

--- animation.js
+++ animation.js
@@ -125,3 +125,18 @@ for (let i = 0; i < paths3.length; i++) {
     });
 
 }
+
+const slidesContainer = document.getElementById("slides-container");
+const slide = document.querySelector(".slide");
+const prevButton = document.getElementById("slide-arrow-prev");
+const nextButton = document.getElementById("slide-arrow-next");
+
+nextButton.addEventListener("click", () => {
+    const slideWidth = slide.clientWidth;
+    slidesContainer.scrollLeft += slideWidth;
+});
+
+prevButton.addEventListener("click", () => {
+    const slideWidth = slide.clientWidth;
+    slidesContainer.scrollLeft -= slideWidth;
+});
